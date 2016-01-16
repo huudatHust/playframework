23b7545231f6b9804a0d57c817fc9ea834c77516
huudatHust <datk571102@gmail.com>
2016-01-16 23:22:46 +0700

    added create functionality to Products

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -3,21 +3,27 @@ package controllers;
 import java.util.List;
 
 import models.Product;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Products extends Controller{
+	private static final Form<Product> productForm = Form.form(Product.class);
 	public static Result list(){
 		List<Product> products = Product.findAll();
 		return ok(views.html.products.list.render(products));
 	}
 	public static Result newProduct(){
-		return TODO;
+		return ok(views.html.products.details.render(productForm));
 	}
 	public static Result details(String ean){
 		return TODO;
 	}
 	public static Result save(){
-		return TODO;
+		Form<Product> boundForm = productForm.bindFromRequest();
+		Product product = boundForm.get();
+		product.save();
+		return  ok(String.format("Saved product %s", product));
 	}
+	
 }
